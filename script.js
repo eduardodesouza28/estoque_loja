cda719701c00cc24e51571ccd32da4275c69a3d1
EDUARDO DE SOUZA <eduardo_souza28@estudante.sesisenai.org.br>
2025-08-19 15:57:18 -0300

    telas feitas

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 class Produto {
-    constructor(produto,entradas,saidas,qtd) {
+    constructor(produto, entradas, saidas, qtd) {
         this.produto = produto;
         this.entradas = entradas;
         this.saidas = saidas;
         this.qtd = qtd;
-        this.situação = qtd < 5? "comprar" : "ok";
+        this.situacao = qtd < 5 ? "comprar" : "ok";
     }
 }
 let regua = new Produto("regua", 6, 4, 2)
@@ -15,16 +15,47 @@ let caneta = new Produto("caneta", 125, 11, 114)
 let papel = new Produto("papel", 10, 4, 6)
 
 let produtos = [regua, lapis, caderno, cola, caneta, papel];
-// let pedidos = [{ data, tipo, descrição, produto, qtd }];
-
 const dropdown = document.getElementById("produtos");
+let quantidade = 0;
 
-produtos.forEach(produto => {
+if (window.location.pathname.endsWith("entradas_saidas.html")) {
+    produtos.forEach(produto => {
     const optionElement = document.createElement("option");
     optionElement.textContent = produto.produto;
     dropdown.appendChild(optionElement);
-});
+});}
 
 function irParaEstoque() {
     window.location.href = "estoque.html";
 }
+
+function irParaLancamentos() {
+    window.location.href = "entradas_saidas.html";
+}
+
+// function getValue() {
+//     quantidade = document.getElementById("quantidade").value;
+
+//     console.log(quantidade)
+// }
+
+
+if (window.location.pathname.endsWith("estoque.html")) {
+    function renderTabela() {
+        let tbody = document.getElementById("tabela-produtos");
+        tbody.innerHTML = produtos.map(p => `
+      <tr>
+        <td>${p.produto}</td>
+        <td>${p.entradas}</td>
+        <td>${p.saidas}</td>
+        <td>${p.qtd}</td>
+        <td>${p.situacao}</td>
+        <td>
+          <span class="alarme ${p.situacao == "comprar" ? "vermelho" : "verde"}"></span>
+        </td>
+      </tr>
+    `).join("");
+    }
+}
+
+renderTabela();
